42224d0ea7f433c78d05880c058abcdeaa72cb25
Nicole Tingey <nicole.tingey1@gmail.com>
2016-11-29 16:15:48 -0700

    Changed the nav bar and working on shopping cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'home', to:'pages#home'
-
-  get 'about', to:'pages#about'
+  root 'pages#home'
 
   get 'contact', to:'pages#contact'
 
@@ -11,7 +9,7 @@ Rails.application.routes.draw do
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
-  mount Spree::Core::Engine, at: '/'
+  mount Spree::Core::Engine, at: '/shop'
           # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
